fc70f4dd6af4ad5f091b25510c3135a6b59c6997
Akash <95037464+AkashsRepositories@users.noreply.github.com>
2022-06-29 01:10:59 +0530

    Time: 4 ms (84.61%), Space: 43.5 MB (65.67%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -3,28 +3,20 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        HashMap<Character, Integer> occurrences1 = new HashMap<>();
-        HashMap<Character, Integer> occurrences2 = new HashMap<>();
-
-        
+       
+        int []arr = new int[26];
         for(int i = 0; i<s.length(); i++){
             char c = s.charAt(i);
-            
-            if(occurrences1.containsKey(c)){
-                occurrences1.put(c, occurrences1.get(c)+1);
-            } else {
-                occurrences1.put(c, 1);
-            }
-            
             char d = t.charAt(i);
-            if(occurrences2.containsKey(d)){
-                occurrences2.put(d, occurrences2.get(d)+1);
-            } else {
-                occurrences2.put(d, 1);
-            }
+            ++arr[c - 'a'];
+            --arr[d - 'a'];
         }
         
+        for(int val: arr){
+            if(val != 0)
+                return false;
+        }
         
-        return occurrences1.equals(occurrences2);
+        return true;
     }
 }
