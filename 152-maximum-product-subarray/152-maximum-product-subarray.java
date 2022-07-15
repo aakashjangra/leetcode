54134cff918a493301b81cdc3304cdf781264524
Akash <95037464+AkashsRepositories@users.noreply.github.com>
2022-07-16 04:01:22 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (97.40%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -5,10 +5,11 @@ class Solution {
         int max = 0;
         int curr = 1;
         for(int i = 0; i<n; i++){
-            curr = curr*nums[i];
+            
             if(nums[i] == 0){
                 curr = 1;
             } else {
+               curr = curr*nums[i];
                max = Math.max(max, curr);
             }
             
@@ -16,10 +17,10 @@ class Solution {
         
         curr = 1;
         for(int i = n-1; i>=0; i--){
-            curr = curr*nums[i];
             if(nums[i] == 0){
                 curr = 1;
             } else {
+                 curr = curr*nums[i];
                  max = Math.max(max, curr);
             }
             
