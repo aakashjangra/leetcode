015efaf4039faf55f6f2bbfdee1b1669f50bc595
Akash <95037464+AkashsRepositories@users.noreply.github.com>
2022-07-28 19:31:45 +0530

    Time: 41 ms (17.25%), Space: 59.3 MB (21.00%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,21 +1,24 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if(strs.length  == 0){
-            return new ArrayList();
-        }
-        Map<String, List> ans = new HashMap<>();
-        for(String str : strs){
-            char []arr = str.toCharArray();
-            Arrays.sort(arr);
-            String sorted = String.valueOf(arr);
+       
+        if(strs.length == 0) return new ArrayList();
+        
+        Map<String, List> ans = new HashMap<String, List>();
+        int []count = new int[26];
+        for(String s : strs){
+            Arrays.fill(count, 0);
+            for(char c: s.toCharArray())  count[c - 'a']++;
             
-            if(!ans.containsKey(sorted)){
-                ans.put(sorted, new ArrayList());
+            StringBuilder sb = new StringBuilder("");
+            for(int i = 0; i<26; i++){
+                sb.append('#');
+                sb.append(count[i]);
             }
-            ans.get(sorted).add(str);
+            String key = sb.toString();
+            if(!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
         }
         
         return new ArrayList(ans.values());
-        
     }
 }
