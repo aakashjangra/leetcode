a49dd8330133bcb14cf7074dcc0793a5e28fc914
Akash <95037464+AkashsRepositories@users.noreply.github.com>
2022-07-29 13:31:10 +0530

    Time: 2 ms (74.25%), Space: 42.7 MB (69.60%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.java
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.java
@@ -1,33 +1,38 @@
 class Solution {
-    public List<String> findAndReplacePattern(String[] words, String pattern) {
-        List<String> ans = new ArrayList();
-
-        for(String str: words){
-            Map<Character, Character> patt = new HashMap();
+    public boolean ifMatches(String str, String pattern){
             Map<Character, Character> curr = new HashMap();
-            
-            boolean flag = true;
             
             for(int i = 0; i<str.length(); i++){
                 char c = str.charAt(i);
                 char p = pattern.charAt(i);
-                if(patt.containsKey(p)){
-                    if(patt.get(p) != c){
-                        flag = false;
-                    }
-                } else {
-                    patt.put(p, c);
-                }
+                
                 if(curr.containsKey(c)){
                     if(curr.get(c) != p){
-                        flag = false;
+                        return false;
                     }
                 } else {
                     curr.put(c, p);
                 }
             }
+        
+            boolean []seen = new boolean[26];    
+            for(char c: curr.values()){
+                if(seen[c-'a']){
+                    return false;
+                } else {
+                    seen[c-'a'] = true;
+                }
+            }
+        
+        return true;
+    }
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> ans = new ArrayList();
+
+        for(String str: words){
             
-            if(flag){
+            
+            if(ifMatches(str, pattern)){
                 ans.add(str);
             }
     
