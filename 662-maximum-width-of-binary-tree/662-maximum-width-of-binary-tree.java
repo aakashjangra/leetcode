970d8503a31af188768d5cf79b411e1033c5816c
Akash <aakashjangra2002@gmail.com>
2022-08-04 11:56:21 +0530

    Time: 2 ms (87.48%), Space: 44 MB (31.62%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -14,27 +14,46 @@
  * }
  */
 class Solution {
-    
-    private int getWidth(TreeNode root, int level, int index, ArrayList<Integer> start, ArrayList<Integer> end){
-        if(root == null) return 0;
-        
-        if(start.size() == level){
-            start.add(index);
-            end.add(index);
-        } else {
-            end.add(level, index);
+    class Pair{
+        TreeNode node;
+        int num;
+        Pair(TreeNode n, int val){
+            node = n;
+            num = val;
         }
-        
-        int curr = end.get(level) - start.get(level) + 1;
-        int left = getWidth(root.left, level+1, index*2, start, end);
-        int right = getWidth(root.right, level+1, (index*2) + 1, start, end);
-        
-        return Math.max(curr, Math.max(left, right));
     }
-    
     public int widthOfBinaryTree(TreeNode root) {
         
-        return getWidth(root, 0, 1, new ArrayList<Integer>(), new ArrayList<Integer>());
+        int ans = 0;
         
+        Queue<Pair> levelOrder = new LinkedList<>();
+        
+        levelOrder.offer(new Pair(root, 0));
+        
+        while(!levelOrder.isEmpty()){
+            int size = levelOrder.size();
+            int lmin = levelOrder.peek().num;
+            int first = 0, last = 0;
+                
+            for(int i = 0; i<size; i++){
+                int curr = levelOrder.peek().num-lmin;
+                TreeNode currNode = levelOrder.peek().node;
+                levelOrder.poll();
+                
+                if(i == 0) first = curr;
+                if(i == size-1) last = curr;
+                
+                if(currNode.left != null){
+                    levelOrder.offer(new Pair(currNode.left, (curr*2)+1));
+                }
+                if(currNode.right != null){
+                    levelOrder.offer(new Pair(currNode.right, (curr*2)+2));
+                }
+            }
+            
+            ans = Math.max(ans, last-first+1);
+        }
+            
+        return ans;
     }
 }
