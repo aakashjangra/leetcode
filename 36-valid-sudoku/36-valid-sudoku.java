a547691d867dbe9102a75310e049225ad918f855
Akash <aakashjangra2002@gmail.com>
2022-08-08 00:29:22 +0530

    Time: 26 ms (20.91%), Space: 48.5 MB (16.67%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -1,66 +1,21 @@
 class Solution {
-    
-    private boolean validRows(char[][] board){
-        int n = 9;
-        for(int i = 0; i<n; i++){
-            HashSet<Character> set = new HashSet<>();
-            for(int j = 0; j<n; j++){
-                if(board[i][j] != '.' && set.contains(board[i][j]))
-                   return false;
-                set.add(board[i][j]);
-            }
-        }
-        
-        return true;
-    }
-
-    private boolean validCols(char[][] board){
-        int n = 9;
-        for(int j = 0; j<n; j++){
-            HashSet<Character> set = new HashSet<>();
-            for(int i = 0; i<n; i++){
-                if(board[i][j] != '.' && set.contains(board[i][j]))
-                   return false;
-                set.add(board[i][j]);
-            }
-        }
-        
-        return true;
-    }
-
-    private boolean validSubs(char[][] board){
-        int n = 9;
-        HashSet<Character> set1 = new HashSet<>();
-        HashSet<Character> set2 = new HashSet<>();
-        HashSet<Character> set3 = new HashSet<>();
-        for(int i = 0; i<n; i++){
-            if(i == 3 || i == 6){
-                set1 = new HashSet<>();
-                set2 = new HashSet<>();
-                set3 = new HashSet<>();
-            }
-            for(int j = 0; j<3; j++){
-                if(board[i][j] != '.' && set1.contains(board[i][j]))
-                    return false;
-                set1.add(board[i][j]);
-            }
-            for(int j = 3; j<6; j++){
-                if(board[i][j] != '.' && set2.contains(board[i][j]))
-                    return false;
-                set2.add(board[i][j]);
-            }
-            for(int j = 6; j<n; j++){
-                if(board[i][j] != '.' && set3.contains(board[i][j]))
-                    return false;
-                set3.add(board[i][j]);
-            }
-        }
-        return true;
-    }
 
     public boolean isValidSudoku(char[][] board) {
         
-        return validRows(board) && validCols(board) && validSubs(board);
+        Set set = new HashSet();
         
+        for(int i = 0; i<9; i++){
+            for(int j = 0; j<9; j++){
+                char c = board[i][j];
+                if(c != '.'){
+                    if(!set.add("row"+c+i) ||
+                       !set.add("col"+c+j) ||
+                       !set.add("submatrix"+c+i/3+' '+j/3))
+                        return false;
+                }
+            }
+        }
+        
+        return true;
     }
 }
