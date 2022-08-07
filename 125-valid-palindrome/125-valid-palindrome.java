28e7d21925ef9e59e791a905a41b3a761a7e132f
Akash <aakashjangra2002@gmail.com>
2022-08-07 13:59:28 +0530

    Time: 4 ms (89.85%), Space: 43.9 MB (55.29%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,22 +1,28 @@
 class Solution {
+    private boolean alphaNumeric(char c){
+        if((48<=c && c<=57) || (97 <= c && c <= 122))
+            return true;
+        return false;
+    }
     public boolean isPalindrome(String s) {
         int n = s.length();
-        
-        String onlyLowerCase = s.toLowerCase();
-        
-        String onlyLowerAlphanum = "";
-        
-        for(int i = 0; i<n; i++){
-            char c = onlyLowerCase.charAt(i);
-            if((48 <= c && c <= 57) || (97 <= c && c <= 122))
-                onlyLowerAlphanum = onlyLowerAlphanum + c;
-        }
-
+        s = s.toLowerCase();
         int front = 0;
-        int back = onlyLowerAlphanum.length() - 1;
+        int back = n - 1;
         
         while(front < back){
-            if(onlyLowerAlphanum.charAt(front) != onlyLowerAlphanum.charAt(back))
+            char charf = s.charAt(front);
+            while(front < n-1 && alphaNumeric(s.charAt(front)) == false)
+                front++;
+            
+            while(back > 0 && alphaNumeric(s.charAt(back)) == false)
+                back--;
+            
+            if(front > back)
+                break;
+            
+            
+            if(s.charAt(front) != s.charAt(back))
                 return false;
             front++;
             back--;
