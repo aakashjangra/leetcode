a6cd87a345296454ce33ac0a81ae964b2ec7bbb6
Akash <aakashjangra2002@gmail.com>
2022-08-12 10:52:49 +0530

    Time: 88 ms (8.89%), Space: 69.3 MB (6.58%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -10,14 +10,7 @@ class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         List<Integer> ans = new ArrayList<>();
         PriorityQueue<Pair> maxHeap = new PriorityQueue<>(
-            new Comparator<Pair>(){
-                public int compare(Pair a, Pair b){
-                    //if both diff are same then compare on the basis of their values
-                    if(a.diff == b.diff)
-                        return b.val - a.val;
-                    return b.diff - a.diff;
-                }
-            }
+            (a, b) -> a.diff == b.diff? b.val - a.val: b.diff - a.diff
         );
         
         // 2 values needs to be passed in Heap and should be sorted based on first value
