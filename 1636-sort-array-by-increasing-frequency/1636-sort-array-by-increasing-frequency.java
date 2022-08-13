800220b07c1221158cf10608ff22260c6af0c213
Akash <aakashjangra2002@gmail.com>
2022-08-13 14:43:56 +0530

    Time: 7 ms (85.29%), Space: 44.7 MB (55.18%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -1,34 +1,26 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b)-> a[0] != b[0]? a[0] - b[0]: b[1] - a[1]);
+        
         for(int a: nums){
             map.put(a, map.getOrDefault(a, 0) + 1);
         }
         
-        int[][] matrix = new int[map.size()][2];
-        int index = 0;
         for(int key: map.keySet()){
-            matrix[index][0] = map.get(key);
-            matrix[index][1] = key;
-            index++;
+            maxHeap.add(new int[]{map.get(key), key});
         }
         
-        Arrays.sort(matrix, ((a,b) -> a[0] != b[0]? a[0] - b[0] : b[1] - a[1]));
-        
-//         for(int []arr: matrix){
+        int idx = 0;
+        while(maxHeap.size() > 0){
+            int freq = maxHeap.peek()[0];
+            int val = maxHeap.peek()[1];
             
-//             System.out.println(arr[0]+" "+arr[1]);
-            
-//         }
-        
-        index = 0;
-        for(int[] arr: matrix){
-            int freq = arr[0];
-            int num = arr[1];
-            while(freq > 0){
-                nums[index++] = num;
-                freq--;
+            while(freq-- > 0){
+                nums[idx++] = val;            
             }
+            
+            maxHeap.poll();
         }
         
         return nums;
