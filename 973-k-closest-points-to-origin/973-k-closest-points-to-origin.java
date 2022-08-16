452c04c7fb6d64af711a381ee4a880f2170e4fe2
Akash <aakashjangra2002@gmail.com>
2022-08-16 12:26:59 +0530

    Time: 70 ms (28.10%), Space: 118.8 MB (19.82%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -9,16 +9,13 @@ class Solution {
             this.y = y;
         }
     }
-    private int distFromOrigin(int x1, int y1){ 
-        //x2 and y2 == 0, as we are finding distance from origin
-        return (int)(Math.pow(x1, 2) + Math.pow(y1, 2));
-    }
+
     public int[][] kClosest(int[][] points, int k) {
         PriorityQueue<myVar> maxHeap = new PriorityQueue<myVar>((a,b) -> b.dist - a.dist);
         for(int[] coordinates: points){
             int x = coordinates[0];
             int y = coordinates[1];
-            maxHeap.add(new myVar(distFromOrigin(x, y), x, y));
+            maxHeap.add(new myVar((x*x)+(y*y), x, y));
             if(maxHeap.size() > k)
                 maxHeap.poll();
         }
