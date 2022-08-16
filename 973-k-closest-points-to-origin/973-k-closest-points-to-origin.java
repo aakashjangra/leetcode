938af828b5179a80a081ea8748f7d641be5b1946
Akash <aakashjangra2002@gmail.com>
2022-08-16 12:28:57 +0530

    Time: 65 ms (32.41%), Space: 117.3 MB (57.77%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,21 +1,11 @@
 class Solution {
-    private class myVar{
-        int dist;
-        int x; 
-        int y;
-        public myVar(int d, int x, int y){
-            this.dist = d;
-            this.x = x;
-            this.y = y;
-        }
-    }
 
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<myVar> maxHeap = new PriorityQueue<myVar>((a,b) -> b.dist - a.dist);
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<int[]>((a,b) -> b[0] - a[0]);
         for(int[] coordinates: points){
             int x = coordinates[0];
             int y = coordinates[1];
-            maxHeap.add(new myVar((x*x)+(y*y), x, y));
+            maxHeap.add(new int[]{(x*x)+(y*y), x, y});
             if(maxHeap.size() > k)
                 maxHeap.poll();
         }
@@ -23,8 +13,8 @@ class Solution {
         int[][] ans = new int[k][2]; 
         int idx = 0;
         while(maxHeap.size() > 0){
-            ans[idx][0] = maxHeap.peek().x;
-            ans[idx][1] = maxHeap.peek().y;
+            ans[idx][0] = maxHeap.peek()[1];
+            ans[idx][1] = maxHeap.peek()[2];
             maxHeap.poll();
             idx++;
         }
