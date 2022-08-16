8bf114b3a1bce7e00fb4381ea0ff5ae9b6b12a0a
Akash <aakashjangra2002@gmail.com>
2022-08-16 17:26:49 +0530

    Time: 7 ms (93.77%), Space: 48.5 MB (71.59%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -6,7 +6,7 @@ class Solution {
             char c = s.charAt(i);
             freq[c-'a']++;
         }
-        
+       
         for(int i = 0; i<n; i++){
             if(freq[s.charAt(i)-'a'] == 1)
                 return i;
