414b8693988fc2e82e5e9cd1e646310ad4ee0dec
Akash <aakashjangra2002@gmail.com>
2022-08-17 11:15:46 +0530

    Time: 3 ms (80.13%), Space: 43.3 MB (6.50%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.java
+++ 804-unique-morse-code-words/804-unique-morse-code-words.java
@@ -1,12 +1,7 @@
 class Solution {
     public int uniqueMorseRepresentations(String[] words) {
-        Map<Character, String> morse_code = new HashMap<>();
+
         String[] table = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        char alphabet = 'a';
-        for(int i = 0; i<26; i++){
-            morse_code.put(alphabet, table[i]);
-            alphabet += 1;
-        }
         
         int transformations = 0;
         Set<String> set = new HashSet<>();
@@ -14,7 +9,7 @@ class Solution {
             int n = str.length();
             String cur_code = "";
             for(int i = 0; i<n; i++){
-                cur_code += morse_code.get(str.charAt(i));
+                cur_code += table[str.charAt(i) - 'a'];
             }
             if(set.add(cur_code))
                 transformations++;
