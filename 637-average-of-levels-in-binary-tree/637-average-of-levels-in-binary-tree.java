2154dadb66b24e228e47a61bcbefb35162553a61
Akash <aakashjangra2002@gmail.com>
2022-09-02 06:47:41 +0530

    Time: 7 ms (9.12%), Space: 47.3 MB (73.52%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
@@ -15,30 +15,41 @@
  */
 class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
+        //queue for levelorder traversal of tree
         Queue<TreeNode> levelOrder = new LinkedList<>();
+        //list of averages according to levels, i.e. 0 index contains average of all nodes at 0th level and so on...
         List<Double> ans = new ArrayList<>();
-        
+        //adding root in queue as we start traversal from level0
         levelOrder.offer(root);
+        //adding null after that so that when null comes we know that it's the end of a level
         levelOrder.offer(null);
+        
+        //intializing sum and total with 0
+        Double sum = 0d;
+        int total = 0;
+        
         while(levelOrder.size() > 1){
             TreeNode current = levelOrder.poll();
-            Double sum = 0d;
-            int total = 0;
-            TreeNode iter = current;
-            while(iter != null){
-                sum += iter.val;
-                total++;
-                if(iter.left != null)
-                    levelOrder.offer(iter.left);
-                if(iter.right != null)
-                    levelOrder.offer(iter.right);
-                iter = levelOrder.poll();
-            }
+            
             if(current != null){
+                sum += current.val;
+                total++;
+                if(current.left != null)
+                    levelOrder.offer(current.left);
+                if(current.right != null)
+                    levelOrder.offer(current.right);
+            } else {
+                //storing average of a level = sum/total
                 ans.add(sum/total);                
                 levelOrder.offer(null);
-            }
+                //resetting for another iteration
+                sum = 0d;
+                total = 0;
+            }            
+            
         }
+        
+        ans.add(sum/total);                
         
         return ans;
     }
