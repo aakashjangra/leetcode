9aaa48e03aa4c6f48dee9a1d38fe944546d0eb07
Akash <aakashjangra2002@gmail.com>
2022-09-06 13:38:10 +0530

    Time: 0 ms (100.00%), Space: 54.2 MB (50.70%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
@@ -14,28 +14,15 @@
  * }
  */
 class Solution {
-    private void helper(TreeNode root, int val, int l, int r){
-        if(root == null)    return;
-        
-        //if leaf node, check if we can attack node with val(value) to it's left or right
-        if(root.left == null && l < val && val < root.val){
-                root.left = new TreeNode(val);
-                return;
-        }
-        
-        if(root.right == null && root.val < val && val < r){
-                root.right = new TreeNode(val);
-                return;
-        }
-        
-        helper(root.left, val, l, root.val);
-        helper(root.right, val, root.val, r);
-    }
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        
         if(root == null)    return new TreeNode(val);
         
-        helper(root, val, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        if(root.val < val){
+            root.right = insertIntoBST(root.right, val);
+        } else {
+            root.left = insertIntoBST(root.left, val);    
+        }
+        
         return root;
     }
 }
