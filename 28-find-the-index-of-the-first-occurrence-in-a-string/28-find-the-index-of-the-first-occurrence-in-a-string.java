d17469850c6b1c31d2df1980f4e26e0b1fdf3e27
Akash <aakashjangra2002@gmail.com>
2022-09-08 10:15:42 +0530

    Time: 1 ms (64.52%), Space: 42.5 MB (15.80%) - LeetHub

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,62 +1,57 @@
 class Solution {
-    // all the character are lowercase so taking a prime no 127(more than double of character set)
-    int prime = 127;
-    public long calPower(int n, int p){
-        if(p == 0)
-            return 1;
-        long pow = n;
-        while(p > 1){
-            pow *= n;
-            p--;
-        }
+    private void createLPS(int[] lps, String pattern, int m){
+        int cur = 1;
+        int ptr = 0;
         
-        return pow;
-    }
-    public long  getHash(String pattern) {
-        
-        int plength = pattern.length();
-        int power = plength -1;
-        long hash = 0;
-         for (int i =0; i<plength ;i++) {
-            int ch = pattern.charAt(i) - 97;
-            hash += calPower(prime,power-- )*ch;
+        while(cur < m){
+            if(pattern.charAt(cur) == pattern.charAt(ptr)){
+                ptr++;
+                lps[cur] = ptr;
+                cur++;
+            } else {
+                if(ptr == 0){
+                    lps[cur] = 0;
+                    cur++;
+                } else {
+                    ptr = lps[ptr-1];
+                }
+            }
         }
-            
-        return hash;
     }
-
     public int strStr(String haystack, String needle) {
-        System.out.println("a test case");
+        int n = haystack.length();
+        int m = needle.length();
         
-        if(haystack.length() < needle.length())
-            return -1;
+        if(n < m)   return -1;
         
-        int k = needle.length();    //size of pattern
-        int start =0, end = k;      //first size of pattern
-        int len = haystack.length();    //len = size of str
-        long patternHash = getHash(needle);   //hash of pattern(double)
-        // System.out.println(patternHash);
+        int[] lps = new int[m];
+        createLPS(lps, needle, m);
         
-        String substr = haystack.substring(0, k);   // first substring of k size
-        // System.out.println(substr);
-        long strHash = getHash(substr);       //hash of first substring
+        //finding needle in haystack using lps
+        int sp = 0; //string pointer
+        int pp = 0; //pattern pointer
         
-        long highestpow = calPower(prime, k-1);   //highest power
-        if (patternHash == strHash) //checking if first substr is equal to pattern
-            return 0;
+        // for(int  a: lps){
+        //     System.out.print(a+" ");
+        // }
+        // System.out.println();
         
-        while (end<len) {   //while end < len, keep window moving
+        while(sp < n){
+            if(haystack.charAt(sp) == needle.charAt(pp)){
+                sp++;
+                pp++;
+            } else {
+                if(pp == 0){
+                    sp++;
+                } else {
+                    pp = lps[pp-1]; 
+                }
+            }
             
-            strHash = (strHash -((int)(haystack.charAt(start)) - 97)* highestpow) *prime +((int)(haystack.charAt(end)) -97);        
-            
-            // System.out.println(strHash);
-            
-            if (patternHash == strHash)
-                return start+1;
-            
-            start++;
-            end++;
+            if(pp == m)
+                return sp-pp;
         }
+        
         return -1;
     }
 }
