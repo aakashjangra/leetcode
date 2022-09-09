7922e0e60108c3999f105c11baa0260b4ee94100
Akash <aakashjangra2002@gmail.com>
2022-09-09 18:21:12 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (73.17%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,21 +1,14 @@
 class Solution {
-    private int rec(int n, int curSteps, int[] dp){
-        if(curSteps == n)
-            return 1;
-        if(curSteps > n)
-            return 0;
-        
-        if(dp[curSteps+1] == 0){
-            dp[curSteps+1] = rec(n, curSteps+1, dp);
-        }
-        if(dp[curSteps+2] == 0){
-            dp[curSteps+2] = rec(n, curSteps+2, dp);
-        }
-        return dp[curSteps+1] + dp[curSteps+2];
-    }
     public int climbStairs(int n) {
         int[] dp = new int[n+2];
         
-        return rec(n, 0, dp);
+        dp[n] = 1;
+        dp[n-1] = 1;
+        
+        for(int i = n-2; i>=0; i--){
+            dp[i] = dp[i+1] + dp[i+2];
+        }
+        
+        return dp[0];
     }
 }
