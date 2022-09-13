c698167d6d62a0f16a98a6ff4449c203c4366c49
Akash <aakashjangra2002@gmail.com>
2022-09-13 21:23:14 +0530

    Time: 11 ms (11.99%), Space: 43 MB (36.91%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,13 +1,17 @@
 class Solution {
     
     private void func(int idx, String str, int n, String digits, HashMap<Character, char[]> map, List<String> ans){
+		//if idx == n, means we have reached the end and taken all the choices. Hence adding the (Str) string made to ans list
         if(idx == n){
             ans.add(str);
             return;
         }
         
+		//taking char array of choices what we have for the current index Digit
         char[] choices = map.get(digits.charAt(idx));
         for(int i = 0; i<choices.length; i++){
+			/*making call to forward digits to make their choices with idx+1 and adding current 
+				character to string being made */
             func(idx+1, str+choices[i], n, digits, map, ans);
         } 
     }
@@ -16,8 +20,10 @@ class Solution {
         
         List<String> ans = new ArrayList<>();
         
+		//if digits string is empty return empty list
         if(digits.length() <= 0)    return ans;
         
+		//mapping values with digits [2-9]
         HashMap<Character, char[]> map = new HashMap<>();
         map.put('2', new char[]{'a', 'b', 'c'});
         map.put('3', new char[]{'d', 'e', 'f'});
@@ -28,6 +34,7 @@ class Solution {
         map.put('8', new char[]{'t', 'u', 'v'});
         map.put('9', new char[]{'w', 'x', 'y', 'z'});
         
+		//calling func to form combinations using backtracking
         func(0, "", digits.length(), digits, map, ans);
         
         return ans;
