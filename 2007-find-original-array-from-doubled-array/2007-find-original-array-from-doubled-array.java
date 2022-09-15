ff03cb277d21fe6092961eef855767712ff870d7
Akash <aakashjangra2002@gmail.com>
2022-09-15 11:03:59 +0530

    Time: 209 ms (48.65%), Space: 125.3 MB (84.28%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,25 +1,31 @@
 class Solution {
     public int[] findOriginalArray(int[] changed) {
         int n = changed.length;
-        int mid = n/2;
         
         // array is empty or array size is odd 
-        if(n == 0 || (n&1) == 1)
+        if((n&1) == 1)
             return new int[]{};
         
+        //sorting to access from smallest to largest elements 
         Arrays.sort(changed);
         
+        //list to store original array numbers
         List<Integer> list = new ArrayList<>();
+        
+        //priority queue to access smallest element whose double needs to be found
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+        //iterating through given array
         for(int a: changed){
+            //if minHeap is empty or the current element is not double of min element available, add current element to heap
             if(minHeap.isEmpty() || minHeap.peek()*2 != a){
                 minHeap.add(a);
-            } else {
+                
+            }   // else don't. as this element is double of element in minHeap
+            else {
                 list.add(minHeap.poll());
-                // System.out.println(a);
             }
         }
-            // System.out.println(list);
         
         if(list.size()*2 != n)
             return new int[]{};
