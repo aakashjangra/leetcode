52c8215c9a0df2847bb65d23b307137cbfa5c987
Akash <aakashjangra2002@gmail.com>
2022-09-20 08:18:53 +0530

    Time: 81 ms (54.89%), Space: 77.8 MB (60.35%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -1,15 +1,15 @@
 class Solution {
     private int[][] dp;
-    private int tryCombinations(int i, int j, int[] nums1, int[] nums2){
-        if(i < 0 || j < 0 || nums1[i] != nums2[j]){
-            return 0;
-        }
+//     private int tryCombinations(int i, int j, int[] nums1, int[] nums2){
+//         if(i >= nums1.length || j >= nums2.length || nums1[i] != nums2[j]){
+//             return 0;
+//         }
         
-        if(dp[i][j] != -1)
-            return dp[i][j];
+//         if(dp[i][j] != -1)
+//             return dp[i][j];
         
-        return dp[i][j] = 1 + tryCombinations(i-1, j-1, nums1, nums2);
-    }
+//         return dp[i][j] = 1 + tryCombinations(i+1, j+1, nums1, nums2);
+//     }
     public int findLength(int[] nums1, int[] nums2) {
         int maxLen = 0;
         
@@ -17,14 +17,16 @@ class Solution {
         int len2 = nums2.length;
         
         dp = new int[len1+1][len2+1];
-        for(int i = 0; i<len1+1; i++){
-            Arrays.fill(dp[i], -1);
-        }
         
-        for(int i = len1-1; i >= 0; i--){
-            for(int j = len2-1; j >= 0; j--){
-                int len = tryCombinations(i, j, nums1, nums2);
-                maxLen = Math.max(len, maxLen);
+        for(int i = len1-1; i>=0; i--){
+            for(int j = len2-1; j>=0; j--){
+                
+                if(nums1[i] != nums2[j]){
+                    continue;    
+                }
+                
+                dp[i][j] = 1 + dp[i+1][j+1];
+                maxLen = Math.max(dp[i][j], maxLen);
             }
         }    
         
