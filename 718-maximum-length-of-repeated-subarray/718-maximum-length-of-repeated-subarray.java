7dd0b381261c31d45073f703da4b416ddc4fba32
Akash <aakashjangra2002@gmail.com>
2022-09-20 16:56:50 +0530

    Time: 351 ms (6.65%), Space: 78.9 MB (29.37%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -1,23 +1,34 @@
 class Solution {
+    private int[][] memo;
+    private int rec(int i, int j, int[] nums1, int[] nums2){
+            if(i >= nums1.length || j >= nums2.length || nums1[i] != nums2[j]){
+                return 0;
+            }
+        
+        if(memo[i][j] != -1){
+            return memo[i][j];
+        }
+        
+        return memo[i][j] =  1 + rec(i+1, j+1, nums1, nums2);
+    }
+    
     public int findLength(int[] nums1, int[] nums2) {
         int len1 = nums1.length;
         int len2 = nums2.length;
         
+        memo = new int[len1+1][len2+1];
+        
+        for(int i = 0; i<len1+1; i++){
+            Arrays.fill(memo[i], -1);
+        }
+        
         int lenMaxSubarray = 0;
         //loop for first array
-        for(int i = 0; i<len1; i++){        // n = 1000, 
-            for(int j = 0; j<len2; j++){    // n * n 
-//                 if(nums1[i] != nums2[j]){
-//                     continue;
-//                 }
-                
-                int tempi = i, tempj = j;
-                int matchedLen = 0;
-                while(tempi < len1 && tempj < len2 && nums1[tempi] == nums2[tempj]){        // n 
-                    tempi++;
-                    tempj++;
-                    matchedLen++;
-                }
+        for(int i = 0; i<len1; i++){        
+            //loop for second array
+            for(int j = 0; j<len2; j++){    
+
+                int matchedLen = rec(i, j, nums1, nums2);
 
                 if(lenMaxSubarray < matchedLen){
                     lenMaxSubarray = matchedLen;
