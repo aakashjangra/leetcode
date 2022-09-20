05ca088246c1e8bca93b2d2dd2a9e777fe084466
Akash <aakashjangra2002@gmail.com>
2022-09-20 08:19:25 +0530

    Time: 47 ms (90.25%), Space: 51.2 MB (83.48%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -26,7 +26,8 @@ class Solution {
                 }
                 
                 dp[i][j] = 1 + dp[i+1][j+1];
-                maxLen = Math.max(dp[i][j], maxLen);
+                
+                if(maxLen < dp[i][j])  maxLen = dp[i][j];
             }
         }    
         
