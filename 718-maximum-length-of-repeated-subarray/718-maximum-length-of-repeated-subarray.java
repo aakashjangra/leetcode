5c8d9d6ccc6b4721742e2a53b9df01bd081a278a
Akash <aakashjangra2002@gmail.com>
2022-09-20 08:02:52 +0530

    Time: 320 ms (7.15%), Space: 78.9 MB (29.37%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -1,25 +1,31 @@
 class Solution {
+    private int tryCombinations(int i, int j, int[] nums1, int[] nums2, int[][] dp){
+        if(i >= nums1.length || j >= nums2.length || nums1[i] != nums2[j]){
+            return 0;
+        }
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        return dp[i][j] = 1 + tryCombinations(i+1, j+1, nums1, nums2, dp);
+    }
     public int findLength(int[] nums1, int[] nums2) {
         int maxLen = 0;
         
         int len1 = nums1.length;
         int len2 = nums2.length;
         
+        int[][] dp = new int[len1+1][len2+1];
+        for(int i = 0; i<len1+1; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        
         for(int i = 0; i<len1; i++){
             for(int j = 0; j<len2; j++){
-                int len = 0;
-                int ptr1 = i;
-                int ptr2 = j;
-                
-                while(ptr1 < len1 && ptr2 < len2 && nums1[ptr1] == nums2[ptr2]){
-                    len++;
-                    ptr1++;
-                    ptr2++;
-                }
-                
-                maxLen = Math.max(maxLen, len);
+                int len = tryCombinations(i, j, nums1, nums2, dp);
+                maxLen = Math.max(len, maxLen);
             }
-        }
+        }    
         
         return maxLen;
     }
