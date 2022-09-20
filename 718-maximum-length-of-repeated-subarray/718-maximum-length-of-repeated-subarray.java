a9e510443f43fad7f839005c6d685d6213261740
Akash <aakashjangra2002@gmail.com>
2022-09-20 08:06:22 +0530

    Time: 324 ms (7.11%), Space: 82 MB (17.14%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -1,14 +1,14 @@
 class Solution {
     private int[][] dp;
     private int tryCombinations(int i, int j, int[] nums1, int[] nums2){
-        if(i >= nums1.length || j >= nums2.length || nums1[i] != nums2[j]){
+        if(i < 0 || j < 0 || nums1[i] != nums2[j]){
             return 0;
         }
         
         if(dp[i][j] != -1)
             return dp[i][j];
         
-        return dp[i][j] = 1 + tryCombinations(i+1, j+1, nums1, nums2);
+        return dp[i][j] = 1 + tryCombinations(i-1, j-1, nums1, nums2);
     }
     public int findLength(int[] nums1, int[] nums2) {
         int maxLen = 0;
@@ -21,8 +21,8 @@ class Solution {
             Arrays.fill(dp[i], -1);
         }
         
-        for(int i = 0; i<len1; i++){
-            for(int j = 0; j<len2; j++){
+        for(int i = len1-1; i >= 0; i--){
+            for(int j = len2-1; j >= 0; j--){
                 int len = tryCombinations(i, j, nums1, nums2);
                 maxLen = Math.max(len, maxLen);
             }
