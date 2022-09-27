53c6b5119f3aa6f8ef5da2b9d3327f496144fb57
Akash <aakashjangra2002@gmail.com>
2022-09-27 09:31:04 +0530

    Time: 44 ms (24.60%), Space: 54 MB (70.30%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.java
+++ 838-push-dominoes/838-push-dominoes.java
@@ -1,53 +1,56 @@
 class Solution {
     public String pushDominoes(String dominoes) {
-        int n = dominoes.length();
-        char[] domino = dominoes.toCharArray();
+        // will calculate the force on every domino in this 
+        //+ve force means domino would fall right and -ve force means domino would fall left
         
-        int[] indexes = new int[n+2];
-        char[] dirs = new char[n+2];
+        int n = dominoes.length(); //length of dominoes array and max. force that can be applied on any domino
+        //calculate right force | +Ve force
+        int force = 0;
+        int[] posForce = new int[n];
+        for(int i = 0; i < n; i++){
+            if(dominoes.charAt(i) == 'R'){
+                force = n;
+            } else if(dominoes.charAt(i) == 'L'){
+                force = 0;
+            }
+            posForce[i] = force;
+            if(force == 0) continue;
+            force--;
+        }
+        
+        //calculate left force | -Ve force
+        force = 0;  // starting force would be zero
+        int[] negForce = new int[n];
+        for(int i = n-1; i >= 0; i--){
+            if(dominoes.charAt(i) == 'L'){
+                force = n;
+            } else if(dominoes.charAt(i) == 'R'){
+                force = 0;
+            }
+            negForce[i] = force; 
+            if(force == 0) continue;
+            force--; //less neg force overtime
+        }
+        
+        //calculating total force and making answer acc. to that
+        //if total force is +ve then domino will fall right => 'R'
+        //if total force is -ve then domino will fall left => 'L'
+        //else it will stand still => '.'
+        StringBuilder ans = new StringBuilder();
 
-        //left most
-        indexes[0] = -1;
-        dirs[0] = 'L';
-        int len = 1;
-        
-        //marking parts
         for(int i = 0; i<n; i++){
-            
-            if(domino[i] != '.'){
-                indexes[len] = i;
-                dirs[len] = domino[i];
-                len++;
+            int totalForce = posForce[i] - negForce[i]; 
+            char current = '.';
+            if(totalForce > 0){
+                current = 'R';
+            } else if(totalForce < 0){
+                current = 'L';
             }
             
+            ans.append(current);
         }
         
-        //right most
-        indexes[len] = n;
-        dirs[len] = 'R';
-      //len represents last Index of Array indexes and dirs 
         
-        //making domino effect
-        for(int index = 0; index<len; index++){
-            int i = indexes[index]+1, j = indexes[index+1]-1;
-            char x = dirs[index], y = dirs[index+1];
-            
-            if(x == y){
-                while(i <= j){
-                    domino[i] = x;
-                    i++;
-                }
-            } else if(x == 'R' && y == 'L'){
-                //half will be filled with 'R' and right half will be filled with 'L'
-                while(i < j){
-                    domino[i] = x;
-                    domino[j] = y;
-                    i++;
-                    j--;
-                }
-            }
-        }
-        
-        return new String(domino);
+        return ans.toString();
     }
 }
