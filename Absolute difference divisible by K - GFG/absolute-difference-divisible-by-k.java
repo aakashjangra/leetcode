86c160cca887538ae3988bf1aa2a70323b00c492
Akash <aakashjangra2002@gmail.com>
2023-01-08 19:36:51 +0530

    Added solution - LeetHub

---
+++ Absolute difference divisible by K - GFG/absolute-difference-divisible-by-k.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main(String args[]) throws IOException {
+		BufferedReader read =
+		new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(read.readLine());
+		while (t-- > 0) {
+			int n = Integer.parseInt(read.readLine());
+			String S[] = read.readLine().split(" ");
+			int[] arr = new int[n];
+			for (int i = 0; i < n; i++)
+				arr[i] = Integer.parseInt(S[i]);
+			int k = Integer.parseInt(read.readLine());
+
+			Solution ob = new Solution();
+			System.out.println(ob.countPairs(n, arr, k));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+	static long countPairs(int n, int[] arr, int k) {
+	    long pairs = (long)0;
+	    Map<Integer, Long> freq = new HashMap<>();
+	    for(int a: arr){
+	        int rem = a%k;
+	        long count = (long)freq.getOrDefault(rem, (long)0);
+	        pairs += count;
+	        
+	        freq.put(rem, count + 1);
+	    }
+	    
+	    return pairs;
+	}
+}
