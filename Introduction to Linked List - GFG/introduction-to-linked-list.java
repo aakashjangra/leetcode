2b2c7f37239fb825f8cb7f17c86d2cc19687299f
Akash <aakashjangra2002@gmail.com>
2023-06-14 20:05:17 +0530

    Added solution - LeetHub

---
+++ Introduction to Linked List - GFG/introduction-to-linked-list.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class Node { 
+    int data; 
+    Node next; 
+
+    Node() { data = 0; }
+    Node(int d) { data = d; }  //constructor to create a new node
+} 
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            Solution obj = new Solution();
+            Node ans = obj.constructLL(a);
+            while (ans != null) {
+                System.out.print(ans.data + " ");
+                ans = ans.next;
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node { 
+    int data; 
+    Node next; 
+
+    Node() { data = 0; }
+    Node(int d) { data = d; }  //constructor to create a new node
+} 
+*/
+class Solution {
+    static Node constructLL(int arr[]){
+        if(arr.length == 0) return null;
+        
+        Node head = new Node(arr[0]);
+        Node ptr = head;
+        
+        for(int i = 1; i<arr.length; i++){
+            int val = arr[i];
+            ptr.next = new Node();
+            ptr = ptr.next;
+            ptr.data = val;
+        }
+        
+        return head;
+    }
+}
