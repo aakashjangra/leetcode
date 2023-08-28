3fff6912628008e627b488eaca1842ec4cfc7ced
Akash <aakashjangra2002@gmail.com>
2023-08-28 11:54:46 +0530

    Added solution - LeetHub

--- Minimum Multiplications to reach End - GFG/minimum-multiplications-to-reach-end.java
+++ Minimum Multiplications to reach End - GFG/minimum-multiplications-to-reach-end.java
@@ -33,39 +33,40 @@ class GFG {
 
 // User function Template for Java
 
-class Pair {
-    int first, second; 
-    Pair(int first, int second) {
-        this.first = first;
-        this.second = second; 
-    }
-}
+
+
+// User function Template for Java
+
 class Solution {
-    int minimumMultiplications(int[] arr, 
-    int start, int end) {
+    int minimumMultiplications(int[] arr, int start, int end) {
         if(start == end) return 0;
-        Queue<Pair> q = new LinkedList<>(); 
-        q.add(new Pair(start, 0)); 
-        int[] dist = new int[100000]; 
-        for(int i = 0;i<100000;i++) dist[i] = (int)(1e9);
-        dist[start] = 0; 
+        
         int mod = 100000;
-        int n = arr.length; 
-        // O(100000 * N) 
-        while(!q.isEmpty()) {
-            int node = q.peek().first; 
-            int steps = q.peek().second;
-            q.remove(); 
-            
-            for(int i = 0;i < n; i++) {
-                int num = (arr[i] * node) % mod; 
-                if(steps + 1 < dist[num]) {
-                    dist[num] = steps + 1; 
-                    if(num == end) return steps + 1; 
-                    q.add(new Pair(num, steps + 1)); 
+        int[] dist = new int[mod];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        //int[] -> {node, steps}
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{start, 0});
+        dist[start] = 0;
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            while(size-- > 0){
+                int[] curPair = q.poll();
+                int cur = curPair[0], steps = curPair[1];
+                
+                for(int a: arr){
+                    int num = (cur * a) % mod;
+                    if(steps+1 < dist[num]){
+                        if(num == end) return steps+1;
+                        
+                        q.add(new int[]{num, steps+1}); 
+                        dist[num] = steps+1; 
+                    } 
                 }
             }
         }
-        return -1; 
+        
+        return -1;
     }
 }
